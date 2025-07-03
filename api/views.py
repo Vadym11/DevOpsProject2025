7b44378e670b3a391a573ddbe677b39f5c6a5010
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2025-07-03 20:47:52 +0100

    basic endpoints added

--- api/views.py
+++ api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+# Book resource
+# /api/books
 # Create your views here.
+
+class HealthView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {
+                "status": "ok"
+            }
+        )
+
+
+class BookView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {
+                "hello": "django"
+            }
+        )
+
+book_view = BookView.as_view()
+
+health_view = HealthView.as_view()
+
