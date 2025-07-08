9523cfcb2190d28f5e989501ab182116ca6ccd00
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2025-07-08 20:04:37 +0100

    fail test

--- api/tests/test_view.py
+++ api/tests/test_view.py
@@ -1,14 +1,37 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
+from api.models import Book
+
+# class BookViewTest(APITestCase):
+#     def test_reponse_is_correct(self):
+#         url = reverse('api:books')
+#         response = self.client.get(url, format='json')
+#         body = response.json()
+#         assert body['hello'] == 'django'
+#         assert response.status_code == status.HTTP_200_OK
 
 class BookViewTest(APITestCase):
     def test_reponse_is_correct(self):
+        book = Book.objects.create(
+            title="Demo",
+            description="Description",
+            author="Author1"
+        )
         url = reverse('api:books')
         response = self.client.get(url, format='json')
         body = response.json()
-        assert body['hello'] == 'django'
         assert response.status_code == status.HTTP_200_OK
+        # this will fail because of created_at field
+        # assert body == [
+        #     {
+        #         "title": book.title,
+        #         "description": book.description,
+        #         "author": book.author
+        #     }
+        # ]
+        assert body[0]['title'] == book.title
+
 
 class HealthViewTest(APITestCase):
     def test_reponse_is_correct(self):
