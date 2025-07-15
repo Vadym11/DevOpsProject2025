56cc5e62ea729e22b151cb02feeb2a35e634d55b
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2025-07-15 15:10:51 +0100

    small comment

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from .models import Book
 
-# use this if want to specify fields
-class BookSerializer(serializers.Serializer):
-    title = serializers.CharField()
+# use this if want to specify fields manually
+# class BookSerializer(serializers.Serializer):
+#     title = serializers.CharField()
 
 
 class BookSerializer(serializers.ModelSerializer):
